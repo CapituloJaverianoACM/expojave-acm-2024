f05ee7c08bf5d80604b9222fc558e86fa85849c6
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-05-01 13:17:10 -0500

    Preguntas actualizadas

--- src/data/preguntas.ts
+++ src/data/preguntas.ts
@@ -1,125 +1,363 @@
 import  { Pregunta }  from '../models/preguntaModel.ts';
 
 export const preguntas: Pregunta[] = [
-  {
-    id: 1,
-    pregunta: "¿Cuál es la capital de Francia?",
-    opcion: ["Madrid", "Berlín", "París", "Lisboa"],
-    respuesta: "París"
-  },
-  {
-    id: 2,
-    pregunta: "¿Cuánto es 2 + 2?",
-    opcion: ["3", "4", "5", "6"],
-    respuesta: "4"
-  },
-  {
-    id: 3,
-    pregunta: "¿Cuál es la capital de Portugal?",
-    opcion: ["Madrid", "Berlín", "París", "Lisboa"],
-    respuesta: "Lisboa"
-  },
-  {
-    id: 4,
-    pregunta: "¿Quién escribió 'Cien años de soledad'?",
-    opcion: ["Gabriel García Márquez", "Mario Vargas Llosa", "Jorge Luis Borges", "Julio Cortázar"],
-    respuesta: "Gabriel García Márquez"
-  },
-  {
-    id: 5,
-    pregunta: "¿Qué elemento químico tiene el símbolo 'O'?",
-    opcion: ["Oro", "Osmio", "Oxígeno", "Oligisto"],
-    respuesta: "Oxígeno"
-  },
-  {
-    id: 6,
-    pregunta: "¿Cuál es el río más largo del mundo?",
-    opcion: ["Nilo", "Amazonas", "Yangtsé", "Misisipi"],
-    respuesta: "Amazonas"
-  },
-  {
-    id: 7,
-    pregunta: "¿Cuál es el planeta más grande del sistema solar?",
-    opcion: ["Saturno", "Júpiter", "Urano", "Neptuno"],
-    respuesta: "Júpiter"
-  },
-  {
-    id: 8,
-    pregunta: "¿En qué año cayó el muro de Berlín?",
-    opcion: ["1989", "1991", "1987", "1990"],
-    respuesta: "1989"
-  },
-  {
-    id: 9,
-    pregunta: "¿Quién pintó la 'Mona Lisa'?",
-    opcion: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],
-    respuesta: "Leonardo da Vinci"
-  },
-  {
-    id: 10,
-    pregunta: "¿Cuál es el deporte más practicado en el mundo?",
-    opcion: ["Baloncesto", "Voleibol", "Críquet", "Fútbol"],
-    respuesta: "Fútbol"
-  },
-  {
-    id: 11,
-    pregunta: "¿En qué país se originó el sushi?",
-    opcion: ["China", "Japón", "Corea del Sur", "Tailandia"],
-    respuesta: "Japón"
-  },
-  {
-    id: 12,
-    pregunta: "¿Cuál es el libro más vendido en el mundo después de la Biblia?",
-    opcion: ["El Señor de los Anillos", "Don Quijote de la Mancha", "El Alquimista", "Harry Potter"],
-    respuesta: "Don Quijote de la Mancha"
-  },
-  {
-    id: 13,
-    pregunta: "¿Quién es el autor de 'Hamlet'?",
-    opcion: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
-    respuesta: "William Shakespeare"
-  },
-  {
-    id: 14,
-    pregunta: "¿Qué país es conocido como la tierra del sol naciente?",
-    opcion: ["India", "Japón", "Australia", "Nueva Zelanda"],
-    respuesta: "Japón"
-  },
-  {
-    id: 15,
-    pregunta: "¿Qué año comenzó la Segunda Guerra Mundial?",
-    opcion: ["1935", "1941", "1939", "1945"],
-    respuesta: "1939"
-  },
-  {
-    id: 16,
-    pregunta: "¿Qué científico es conocido por la teoría de la relatividad?",
-    opcion: ["Isaac Newton", "Albert Einstein", "Niels Bohr", "Galileo Galilei"],
-    respuesta: "Albert Einstein"
-  },
-  {
-    id: 17,
-    pregunta: "¿Cuál es el metal más caro del mundo?",
-    opcion: ["Oro", "Platino", "Rodio", "Palladium"],
-    respuesta: "Rodio"
-  },
-  {
-    id: 18,
-    pregunta: "¿Cuántos corazones tiene un pulpo?",
-    opcion: ["Uno", "Dos", "Tres", "Cuatro"],
-    respuesta: "Tres"
-  },
-  {
-    id: 19,
-    pregunta: "¿Cuál es el idioma más hablado en el mundo?",
-    opcion: ["Español", "Inglés", "Chino Mandarín", "Hindi"],
-    respuesta: "Chino Mandarín"
-  },
-  {
-    id: 20,
-    pregunta: "¿Cuál es el país más poblado del mundo?",
-    opcion: ["Estados Unidos", "India", "China", "Indonesia"],
-    respuesta: "China"
-  }
+    {
+        "id": 1,
+        "pregunta": "¿Por qué se debería preferir el protocolo TCP sobre UDP en un chat en tiempo real?",
+        "opcion": [
+            "Porque TCP asegura que lleguen los paquetes de red.",
+            "No se debe preferir TCP.",
+            "Porque TCP es más seguro.",
+            "A y C son correctas."
+        ],
+        "respuesta": "No se debe preferir TCP."
+    },
+    {
+        "id": 2,
+        "pregunta": "¿Qué papel juega un VPN en la seguridad de las redes?",
+        "opcion": [
+            "Acelerar la velocidad de conexión a Internet.",
+            "Encriptar el tráfico de datos para proteger la privacidad.",
+            "Gestionar el control de versiones de los proyectos de programación.",
+            "Compilar el código fuente en programas ejecutables."
+        ],
+        "respuesta": "Encriptar el tráfico de datos para proteger la privacidad."
+    },
+    {
+        "id": 3,
+        "pregunta": "¿Qué es github?",
+        "opcion": [
+            "Una plataforma colaborativa para alojamiento de proyectos de inteligencia artificial que permite la interacción en tiempo real",
+            "Un sistema integral de gestión de proyectos y seguimiento de errores utilizado principalmente en el desarrollo de software",
+            "Un sistema de control de versiones de código abierto utilizado para colaborar en proyectos de programación",
+            "Una red social para desarrolladores que facilita la discusión y el intercambio de scripts y programas a nivel global"
+        ],
+        "respuesta": "Un sistema de control de versiones de código abierto utilizado para colaborar en proyectos de programación"
+    },
+    {
+        "id": 4,
+        "pregunta": "¿Cuál es el propósito principal de un compilador en el contexto de la programación?",
+        "opcion": [
+            "Facilitar la conexión segura a una red VPN.",
+            "Traducir nombres de dominio a direcciones IP.",
+            "Traducir codigo a un ejecutale.",
+            "Encriptar el tráfico de datos para proteger la privacidad."
+        ],
+        "respuesta": "Traducir codigo a un ejecutale."
+    },
+    {
+        "id": 5,
+        "pregunta": "¿Cómo es conocido el malware caracterizado por el bloqueo de acceso al sistema o de archivos exigiendo para su liberación un pago monetario?",
+        "opcion": [
+            "Adware",
+            "Phishing",
+            "Spoofing",
+            "Ransomware"
+        ],
+        "respuesta": "Ransomware"
+    },
+    {
+        "id": 6,
+        "pregunta": "¿Cómo se llama la primera plataforma P2P (Peer-To-Peer) de distribución de archivos de música? Precidió a lo que hoy en día es Spotify, Apple Music, etc.",
+        "opcion": [
+            "Sony",
+            "Napster",
+            "Piratebay",
+            "YouTube"
+        ],
+        "respuesta": "Napster"
+    },
+    {
+        "id": 7,
+        "pregunta": "¿Cuál es la diferencia entre JavaScript y TypeScript?",
+        "opcion": [
+            "JS no acepta Programación Orientada a Objetos (POO) mientras que TS si",
+            "JS es interpretado, mientras que TS añade tipado.",
+            "No hay ninguna diferencia entre ambos.",
+            "TS es interpretado, mientras que JS añade tipado."
+        ],
+        "respuesta": "JS es interpretado, mientras que TS añade tipado."
+    },
+    {
+        "id": 8,
+        "pregunta": "¿Cuál es el nombre del primer videojuego de realidad virtual que ganó un premio Oscar?",
+        "opcion": [
+            "Half-Life: Alyx",
+            "Resident Evil VII",
+            "It takes Two",
+            "Baldur’s Gate III"
+        ],
+        "respuesta": "Half-Life: Alyx"
+    },
+    {
+        "id": 9,
+        "pregunta": "¿Qué es un CDN en redes y telecomunicaciones?",
+        "opcion": [
+            "Una red de distribución de contenido que entrega de datos de manera rápida.",
+            "Una cadena de televisión.",
+            "Círculo de Datos Nobles es una red de distribución de información para pasar datos a bajo costo.",
+            "Un CDN es un Camino de Datos Nublados, un sendero virtual donde la información se pierde en la neblina digital."
+        ],
+        "respuesta": "Una red de distribución de contenido que entrega de datos de manera rápida."
+    },
+    {
+        "id": 10,
+        "pregunta": "¿Cuál era el nombre del protagonista principal de Grand Theft Auto San Andreas?",
+        "opcion": [
+            "Michael De Santa.",
+            "Carl Jenkins.",
+            "Cesar Vialpando.",
+            "Carl Johnson"
+        ],
+        "respuesta": "Carl Johnson"
+    },
+    {
+        "id": 11,
+        "pregunta": "¿Qué es una dirección IP?",
+        "opcion": [
+            "Un identificador para mi casa en internet.",
+            "Un identificador numérico para cada dispositivo que usa internet.",
+            "Un identificador alfanumérico sólo para computadores que usan internet.",
+            "Un identificador para saber que tan cerca o lejos estoy de una señal de Wi-Fi."
+        ],
+        "respuesta": "Un identificador numérico para cada dispositivo que usa internet."
+    },
+    {
+        "id": 12,
+        "pregunta": "¿A que corresponde las siglas 'IHS' en el escudo de la universidad?",
+        "opcion": [
+            "A nada, es porque se veía lindo.",
+            "Corresponde al nombre pasado de la universidad.",
+            "Es la abreviatura del nombre de Jesus escrito en Griego.",
+            "Abreviatura de la fundación de la universidad."
+        ],
+        "respuesta": "Es la abreviatura del nombre de Jesus escrito en Griego."
+    },
+    {
+        "id": 13,
+        "pregunta": "¿Cuál de los siguientes conceptos NO es un principio de la programación orientada a objetos?",
+        "opcion": [
+            "Encapsulamiento",
+            "Herencia",
+            "Polimorfismo",
+            "Iteración"
+        ],
+        "respuesta": "Iteración"
+    },
+    {
+        "id": 14,
+        "pregunta": "¿Cuál de los siguientes patrones de diseño se utiliza comúnmente para crear objetos complejos de manera eficiente y flexible en la programación orientada a objetos?",
+        "opcion": [
+            "Singleton",
+            "Factory Method",
+            "Observer",
+            "Decorator"
+        ],
+        "respuesta": "Factory Method"
+    },
+    {
+        "id": 15,
+        "pregunta": "¿Cuál es la mejor descripción del principio de encapsulamiento en la programación orientada a objetos?",
+        "opcion": [
+            "Ocultar detalles internos y permitir acceso a través de métodos públicos.",
+            "Heredar propiedades y comportamientos de una clase base.",
+            "Adaptarse a diferentes tipos de datos.",
+            "Interactuar con otros objetos de manera dinámica."
+        ],
+        "respuesta": "Ocultar detalles internos y permitir acceso a través de métodos públicos."
+    },
+    {
+        "id": 16,
+        "pregunta": "¿Cuál de los siguientes protocolos se utiliza comúnmente para asegurar las comunicaciones en línea mediante el cifrado de datos entre el navegador web y el servidor web?",
+        "opcion": [
+            "SSL",
+            "HTTP",
+            "FTP",
+            "UDP"
+        ],
+        "respuesta": "SSL"
+    },
+    {
+        "id": 17,
+        "pregunta": "¿Cuál de los siguientes es un lenguaje de marcado utilizado para crear páginas web?",
+        "opcion": [
+            "XML",
+            "JSON",
+            "SQL",
+            "Python"
+        ],
+        "respuesta": "XML"
+    },
+    {
+        "id": 18,
+        "pregunta": "¿Qué sigla representa la unidad básica de almacenamiento en una computadora?",
+        "opcion": [
+            "GB",
+            "MB",
+            "KB",
+            "TB"
+        ],
+        "respuesta": "KB"
+    },
+    {
+        "id": 19,
+        "pregunta": "¿Cuál es una característica principal de Git que no está presente en GitHub?",
+        "opcion": [
+            "Gestión de repositorios remotos.",
+            "Control de versiones distribuido.",
+            "Colaboración en línea.",
+            "Control de acceso y permisos."
+        ],
+        "respuesta": "Control de versiones distribuido."
+    },
+    {
+        "id": 20,
+        "pregunta": "¿Cuál es la principal diferencia entre bases de datos relacionales y no relacionales?",
+        "opcion": [
+            "Estructura de datos.",
+            "Consultas.",
+            "Flexibilidad.",
+            "Escalabilidad."
+        ],
+        "respuesta": "Estructura de datos."
+    },
+    {
+        id: 21,
+        pregunta: "¿Cuál es el libro más vendido en el mundo después de la Biblia?",
+        opcion: ["El Señor de los Anillos", "Don Quijote de la Mancha", "El Alquimista", "Harry Potter"],
+        respuesta: "Don Quijote de la Mancha"
+    },
+    {
+        "id": 22,
+        "pregunta": "¿Cuántos bytes tiene una dirección IPV4 e IPV6?",
+        "opcion": [
+            "IPV4: 32 bits (4 bytes). IPV6: 64 bits (8 bytes)",
+            "IPV4: 64 bits (8 bytes). IPV6: 128 bits (16 bytes)",
+            "IPV4: 16 bits (2 bytes). IPV6: 256 bits (32 bytes)",
+            "IPV4: 32 bits (4 bytes). IPV6: 128 bits (16 bytes)"
+        ],
+        "respuesta": "IPV4: 32 bits (4 bytes). IPV6: 128 bits (16 bytes)"
+    },
+    {
+        "id": 23,
+        "pregunta": "¿Cuál de estos lenguajes es ampliamente utilizado en el desarrollo web tanto en el lado del cliente como en el lado del servidor?",
+        "opcion": [
+            "Python",
+            "JavaScript",
+            "Java",
+            "C++"
+        ],
+        "respuesta": "JavaScript"
+    },
+    {
+        "id": 24,
+        "pregunta": "¿Quién es considerado como el padre de la computación y cuál fue su contribución más significativa?",
+        "opcion": [
+            "Alan Turing.",
+            "Charles Babbage.",
+            "John von Neumann.",
+            "Ada Lovelace."
+        ],
+        "respuesta": "Charles Babbage."
+    },
+    {
+        "id": 25,
+        "pregunta": "¿Cuál de los siguientes algoritmos de ordenamiento tiene una complejidad temporal de O(n^2)?",
+        "opcion": [
+            "Quicksort",
+            "MergeSort",
+            "Bubble Sort",
+            "Heap Sort"
+        ],
+        "respuesta": "Bubble Sort"
+    },
+    {
+        "id": 26,
+        "pregunta": "¿Qué significa LAN en el contexto de redes informáticas?",
+        "opcion": [
+            "Local Area Network",
+            "Longitudinal Access Node",
+            "Logical Application Network",
+            "Latency Adaptive Networking"
+        ],
+        "respuesta": "Local Area Network"
+    },
+    {
+        "id": 27,
+        "pregunta": "¿Qué tecnología es fundamental para la computación en la nube?",
+        "opcion": [
+            "Virtualización",
+            "Inteligencia Artificial",
+            "Blockchain",
+            "Internet de las cosas"
+        ],
+        "respuesta": "Virtualización"
+    },
+    {
+        "id": 28,
+        "pregunta": "¿Cuál de las siguientes opciones es un ejemplo de un ataque de ingeniería social?",
+        "opcion": [
+            "Virus informático.",
+            "Phishing.",
+            "Firewall.",
+            "Encriptación de datos."
+        ],
+        "respuesta": "Phishing."
+    },
+    {
+        "id": 29,
+        "pregunta": "¿Como es conocida la estatua que se situa en frente de la biblioteca principal?",
+        "opcion": [
+            "Rey leon",
+            "Señor en el arbol",
+            "Ignacio del Loyola",
+            "Ninguna de las anteriores."
+        ],
+        "respuesta": "Rey leon"
+    },
+    {
+        "id": 30,
+        "pregunta": "¿Qué tecnología permite a los dispositivos conectarse entre sí sin necesidad de cables?",
+        "opcion": [
+            "Bluetooth.",
+            "NFC (Near Field Communication).",
+            "Wi-Fi.",
+            "Todas las anteriores."
+        ],
+        "respuesta": "Todas las anteriores."
+    },
+    {
+        "id": 31,
+        "pregunta": "¿Cuál de los siguientes juegos fue uno de los primeros en popularizar el género de los juegos de rol de acción?",
+        "opcion": [
+            "Diablo.",
+            "World of Warcraft.",
+            "League of Legends.",
+            "Starcraft."
+        ],
+        "respuesta": "Diablo"
+    },
+    {
+        "id": 32,
+        "pregunta": "¿Qué es HTML en el desarrollo web?",
+        "opcion": [
+            "Un lenguaje de programación.",
+            "Un lenguaje de hoja de estilo.",
+            "Un lenguaje de marcado para diseñar páginas web.",
+            "Un sistema de gestión de bases de datos."
+        ],
+        "respuesta": "Un lenguaje de marcado para diseñar páginas web."
+    },
+    {
+        "id": 33,
+        "pregunta": "¿Cuál de las siguientes opciones describe mejor el origen del término 'bug' en el contexto de la informática?",
+        "opcion": [
+            "Por un insecto atrapado en una computadora.",
+            "Se originó como un término coloquial para describir problemas.",
+            "Por Charles Babbage, para describir errores en sus máquinas analíticas.",
+            "En los primeros días de la computación para referirse a problemas en el software."
+        ],
+        "respuesta": "Por un insecto atrapado en una computadora."
+    }
 ];
 
